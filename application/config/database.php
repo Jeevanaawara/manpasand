a54b6784c9f570148a4998aa37c9173a03772f5b
Jeevan Kumar <jeevanaawara33@gmail.com>
2017-12-12 13:35:14 +0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
+	'hostname' => 'mysql.manpasand.svc',
+	'username' => 'manpasanduser01',
+	'password' => 'manpasanduser01@db',
 	'database' => 'manpasand',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
